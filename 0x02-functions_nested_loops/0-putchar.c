7e36e57cbb293e9b8488e9703afb983e6fbb3000
MariamawitS <mariamawitsimon@gmail.com>
2022-06-08 10:30:54 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - entry block
+ * main - entry block 
  * @void: no argument
  * Return: 0
  **/
@@ -12,7 +12,7 @@ int main(void)
 
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(ermias[c]);
+		_putchar(mariamawit[c]);
 	}
 	_putchar('\n');
 	return (0);
